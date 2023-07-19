61e24a4e3e41266c593f8722d208d879df030f88
tonyr <tonchr.tony@gmail.com>
2023-07-19 17:10:39 +1200

    New Version of the Project

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root to: 'pages#index'
+  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
---
+++ test/controllers/index_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class IndexControllerTest < ActionDispatch::IntegrationTest
+  test "should get contact" do
+    get index_contact_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get index_about_url
+    assert_response :success
+  end
+end
